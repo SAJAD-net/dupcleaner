347cf8a2594b3f78bc02a3ee89c7a41200b9b573
SAJADnet <sajadsajad.sch@gmail.com>
2024-02-04 17:15:00 +0330

    a faster way to get the duplicates

--- dupcleaner.sh
+++ dupcleaner.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/bash
 
+startt=`date +%s`
 
-echo "> recording all the existing files in : `pwd`" 
+echo "> recording all the existing files in : $1" 
 find $1 -type f -print > ".dup.cleaner.files.txt"
 echo -e "< done.\n"
 
 echo "> generating the hashes"
 while IFS=$'\n' read -r line; do
-	md5sum "$line" | cut -d ' ' -f 1 >> ".dup.cleaner.hashes.txt"
+	md5sum "$line" >> ".dup.cleaner.hashes.txt"
 done < .dup.cleaner.files.txt
 echo -e "< done.\n"
 
 echo "> finding the duplicates"
-sort .dup.cleaner.hashes.txt | uniq -d >> .dup.cleaner.duphashes.txt
+awk 'NR==FNR{a[$1]++;next;}{ if (a[$1] > 1)print;}' .dup.cleaner.hashes.txt .dup.cleaner.hashes.txt | sort -u -k1,1 | cut -d ' ' -f 3- > .dup.cleaner.dupfiles.txt
 echo -e "< done.\n"
 
 echo "> deleting the duplicates"
 duplicates=0
 while IFS=$'\n' read -r line; do
-	fhash=$(md5sum "$line" | cut -d ' ' -f 1)
-	if grep "$fhash" .dup.cleaner.duphashes.txt; then
-		echo "deleting : $line"
-		rm "$line"
-		sed -i "/$fhash/d" .dup.cleaner.duphashes.txt
-		duplicates=`expr $duplicates + 1`
-	fi
-done < .dup.cleaner.files.txt
+	echo "deleting : $line"
+	rm "$line"
+	duplicates=`expr $duplicates + 1`
+done < .dup.cleaner.dupfiles.txt
 rm .dup.cleaner.*
-echo -e "\n< done. total duplicate files deleted : $duplicates"
+endt=`date +%s`
+totallt=`expr $endt - $startt`
+echo -e "\n< done.\n"
+echo -e "total duplicate files deleted : $duplicates in $totallt seconds!"
